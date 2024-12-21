92055809f8e4091c3fb2df543143b355ac2c1e39
Anton Kozakov <hamsteramk2017@gmail.com>
2024-12-21 10:53:00 +0300

    Update calculate.py

--- lib/calculate.py
+++ lib/calculate.py
@@ -9,20 +9,25 @@ sizes = {
     'area-square': 1,
 }
 
+
 def calc(fig, func, size):
     # Проверка корректности данных
     if fig not in figs:
-        raise ValueError(f"Invalid figure: {fig}. Available figures are: {figs}")
+        raise ValueError(f"Invalid figure: {fig}. \
+        Available figures are: {figs}")
     if func not in funcs:
-        raise ValueError(f"Invalid function: {func}. Available functions are: {funcs}")
+        raise ValueError(f"Invalid function: {func}. \
+        Available functions are: {funcs}")
     if len(size) != sizes.get(f"{func}-{fig}", 1):
-        raise ValueError(f"Invalid number of parameters for {fig} and {func}. Expected {sizes.get(f'{func}-{fig}', 1)} parameters.")
+        raise ValueError(f"Invalid number of parameters \
+        for {fig} and {func}. \
+        Expected {sizes.get(f'{func}-{fig}', 1)} parameters.")
 
     # Динамический импорт модуля фигуры
     try:
-        module = importlib.import_module(fig)  # Например, 'circle' или 'square'
+        module = importlib.import_module(fig)  
         # Получение функции из модуля и её вызов
-        func_to_call = getattr(module, func)  # Например, 'area' или 'perimeter'
+        func_to_call = getattr(module, func)  
         result = func_to_call(*size)
         return result
     except Exception as e:
