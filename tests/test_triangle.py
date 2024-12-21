5cc8a7069026c45e151193e56ec323b78e65b2a7
Anton Kozakov <hamsteramk2017@gmail.com>
2024-12-21 10:57:49 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -2,6 +2,7 @@ import pytest
 import triangle
 import math
 
+
 def test_triangle_area():
     side1 = 3
     side2 = 4
@@ -11,6 +12,7 @@ def test_triangle_area():
     result = triangle.area(side1, side2, side3)
     assert result == pytest.approx(expected_area)
 
+
 def test_triangle_perimeter():
     side1 = 3
     side2 = 4
@@ -18,5 +20,4 @@ def test_triangle_perimeter():
     expected_perimeter = side1 + side2 + side3
     result = triangle.perimeter(side1, side2, side3)
     assert result == expected_perimeter
-        
-        
+           
